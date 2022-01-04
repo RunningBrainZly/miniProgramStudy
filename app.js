b65c70a33ac14fa8eeea0a86ccec632e06f8b9e0
zly <1271224158@qq.com>
2022-01-04 19:37:58 +0800

    miniProgram base study
    小程序基础学习

--- app.js
+++ app.js
@@ -17,3 +17,40 @@ App({
     userInfo: null
   }
 })
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+  // 页面启动时找不到启动页时候触发
+  onPageNotFound(){
+    // 可以通过这个方法跳转页面，不能挑tabbar界面
+    wx.navigateTo({
+      url: 'url',
+    })
+  }
+})
